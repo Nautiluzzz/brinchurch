16aa29a723b17578f728edd8c6a155287f2295dd
Alexander Brin <xanderdl@gmail.com>
2012-08-15 18:41:43 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brinchurch::Application.config.secret_token = 'c76ebb84dc3c867d7e3a22bee3594e7aa003023330f028af0a661622c4f5b5a3e02e6d1d89d3eaa22ba0d03f5d75e6f59f62e50667c8d328b7dc6df3a149d9f4'
